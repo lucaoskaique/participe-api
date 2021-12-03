6ce43e0c8fac815dce67ef808a812ec7b0d31df4
lucaoskaique <lucas.kaique175@hotmail.com>
2021-12-03 11:53:52 -0300

    commented routes

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -4,24 +4,24 @@ const usersRoutes = Router();
 
 usersRoutes.post("/list");
 
-usersRoutes.post("/", createUserController.handle);
+// usersRoutes.post("/", createUserController.handle);
 
-usersRoutes.put("/", updateUserController.handle);
+// usersRoutes.put("/", updateUserController.handle);
 
-usersRoutes.put("/update_password", updateUserPasswordController.handle);
+// usersRoutes.put("/update_password", updateUserPasswordController.handle);
 
-usersRoutes.get("/profile", listUserController.handle);
+// usersRoutes.get("/profile", listUserController.handle);
 
-usersRoutes.post("/forgot_password", sendForgotPasswordMailController.handle);
+// usersRoutes.post("/forgot_password", sendForgotPasswordMailController.handle);
 
-usersRoutes.post("/reset_password/:token", resetPasswordController.handle);
+// usersRoutes.post("/reset_password/:token", resetPasswordController.handle);
 
-usersRoutes.get("/cria-usuarios", createMultipleUsersController.handle);
+// usersRoutes.get("/cria-usuarios", createMultipleUsersController.handle);
 
-usersRoutes.get(
-  "/login-trocapontos",
+// usersRoutes.get(
+//   "/login-trocapontos",
 
-  loginTrocaPontosController.handle
-);
+//   loginTrocaPontosController.handle
+// );
 
 export { usersRoutes };

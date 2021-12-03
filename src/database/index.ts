4b14e4f72828e5e9e0a656cbe6ecc3ac0f558204
lucaoskaique <lucas.kaique175@hotmail.com>
2021-12-03 11:52:14 -0300

    database configs

---
+++ src/database/index.ts
@@ -0,0 +1,11 @@
+import * as dotenv from "dotenv";
+import mysql from "mysql2";
+
+dotenv.config();
+
+export const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB_NAME,
+});

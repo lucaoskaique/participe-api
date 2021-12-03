be7ff632c0964deffa26d83bc7e067788d735fa5
lucaoskaique <lucas.kaique175@hotmail.com>
2021-12-03 11:53:10 -0300

    created entitie for project

---
+++ src/modules/projects/entities/Project.ts
@@ -0,0 +1,16 @@
+class Project {
+  id: number;
+  project_name: string;
+  project_description: string;
+  city_id: number;
+  start_date: Date;
+  end_date: Date;
+  votes: number;
+  goal: number;
+  project_status_id: number;
+  user_id: number;
+  created_at: Date;
+  update_at: Date;
+}
+
+export { Project };
